06336ba6bdcf1607fc797092bd9cd04b1da714bd
iamchaki <searchingpurpose8@gmail.com>
2026-02-01 12:44:04 +0530

    Refactor app.js to use environment port
    Signed-off-by: iamchaki <searchingpurpose8@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
@@ -53,4 +53,5 @@ app.get('/', (req, res) => {
 // Start Server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+
 });
